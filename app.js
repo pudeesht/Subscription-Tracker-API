00e54caf22f2325cdf31af80016e71de912a9f82
pudeesht <vaibhavsharma01@outlook.com>
2025-08-27 00:34:37 +0530

    Add signin signout routes using JWT

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subRouter from "./routes/subscription.routes.js";
 import connectionToDatabse from './database/mongodb.js';
+import errormiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
 
 
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/subscription",subRouter);
+
+app.use(errormiddleware);
 
 
 app.get('/',(req,res)=>{
@@ -24,7 +32,6 @@ app.listen(PORT,async()=>{
     await connectionToDatabse();
 
 })
-
 
 
 export default app;
